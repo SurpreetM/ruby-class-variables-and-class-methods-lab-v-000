7dd2ce75901dcbcf88f3ac2dcf69acad36a9b2c0
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-08-09 16:49:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ class Song
 
   @@count = 0
   @@genres = []
+  @@artists = []
 
   attr_accessor :name, :artist, :genre
 
@@ -12,7 +13,8 @@ class Song
 
     @@count += 1
     @@genre << genre
-    
+    @@artists << artist
+
   end
 
 
